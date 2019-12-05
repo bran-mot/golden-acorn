6dae82a1c3c7160a310565ce0f101604fdbe54eb
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-12-05 17:02:17 +0100

    add buttons

--- golden-acorn/src/App.js
+++ golden-acorn/src/App.js
@@ -3,22 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function Button(props) {
+    return <button>{props.label}</button>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button label="Buy One"></Button>
+      <Button label="Eat One"></Button>
     </div>
   );
 }
