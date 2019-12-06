2f838a9ed66ebd11e9a55d06bf7572c0fbcc245a
Brandon Motwani <brandon.motwani@gmail.comgit config --global user.email brandon.motwani@gmail.com>
2019-12-06 10:27:44 +0100

    add components

--- golden-acorn/src/App.js
+++ golden-acorn/src/App.js
@@ -1,16 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types';
+import Button from './components/Button'
+import Display from './components/Display'
+
+class SimpleGoldenAcornApp extends React.Component {
+  constructor(){
+    super();
+    this.addOne = this.addOne.bind(this);
+    this.minusOne = this.minusOne.bind(this);
+    this.state = {
+      number: 0
+    };
+  }
+
+  addOne(){
+    this.setState(previousState => {
+      return {
+        number: previousState.number +1
+      }
+    })
+  } 
+
+  minusOne(){
+    this.setState(previousState => {
+      return {
+        number: previousState.number -1
+      }
+    })
+  } 
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.addOne}>Roll One</Button>
+        <Display>{this.state.number}</Display>
+        <Button onClick={this.minusOne}>Smoke one</Button>
+      </div>
+    );
+  }
+}
 
 function App() {
-  function Button(props) {
-    return <button>{props.label}</button>;
-  }
+
+  
+  
 
   return (
     <div className="App">
-      <Button label="Buy One"></Button>
-      <Button label="Eat One"></Button>
+      <SimpleGoldenAcornApp />
     </div>
   );
 }
---
+++ golden-acorn/src/components/Display.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+export default function Display(props) {
+    return <div>{props.children}</div>;
+  }
